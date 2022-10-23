e7332020feb69938816f0701d9d8e03880d68f03
NiuBoss123 <i@niuboss123.com>
2022-10-23 14:00:14 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     ({
     announcementBar: {
       id: 'support_us',
-      content: '本站仍在建設中，您可以關注我們<a href="https://www.bc413.com/categories/acomoe.46/">位於中國大陸的論壇</a>來獲得最新消息,
+      content: '本站仍在建設中，您可以關注我們<a href="https://www.bc413.com/categories/acomoe.46/">位於中國大陸的論壇</a>來獲得最新消息',
       backgroundColor: '#a26ac8',
       textColor: '#fff',
       isCloseable: true,
